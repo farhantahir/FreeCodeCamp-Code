aabe81dc4e63d90325404d204448acaf8b6b6a22
ftahir <ftahir@ssidecisions.com>
2016-10-20 18:46:18 +0500

    Basic Algorithm Scripting 04 Find the Longest Word in a String Solution

--- Basic-Algorithm-Scripting/longest_word_in_a_string.js
+++ Basic-Algorithm-Scripting/longest_word_in_a_string.js
@@ -1,3 +1,22 @@
 /**
- * Created by ftahri on 10/20/16.
+ * @author Farhan Tahir <mf.tahir@outlook.com>
+ * @param str
+ * @returns {*}
+ * @description Find the Longest Word in a String, freecodecamp basic algorithm challenge
  */
+/*
+ Return the length of the longest word in the provided sentence.
+
+ Your response should be a number.
+
+ Remember to use Read-Search-Ask if you get stuck. Write your own code.
+ */
+function findLongestWord(str) {
+    var  strWords = str.split(" ");
+    var longestWordLength = strWords.reduce(function(wordLength,currentWord){
+        return (currentWord.length > wordLength ) ? currentWord.length : wordLength;
+    },0);
+
+    return longestWordLength;
+}
+
