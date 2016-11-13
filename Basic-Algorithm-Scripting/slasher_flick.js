67c07b4edaf97169d2204f7a3c2f038b98dc9698
mf.tahir <mf.tahir@outlook.com>
2016-11-13 23:03:06 +0500

    Slasher Flick, freecodecamp basic algorithm challenge

---
+++ Basic-Algorithm-Scripting/slasher_flick.js
@@ -0,0 +1,9 @@
+/**
+ * @author Farhan Tahir <mf.tahir@outlook.com>
+ * @param  arr
+ * @param  howMany
+ * @description Slasher Flick, freecodecamp basic algorithm challenge
+ */
+function slasher(arr, howMany) {
+  return arr.slice(howMany);
+}
